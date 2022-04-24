961252643ada098e79fffdcae0978e4a789f91ae
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-24 15:29:54 -0600

    many-to-many associations and model setup

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -1,7 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Contestant, type: :model do
-  describe 'relationships' do
-    it {should belong_to :bachelorette}
+  describe "relationships" do
+    it { should belong_to :bachelorette }
+    it { should have_many :outing_contestants }
+    it { should have_many(:outings).through(:outing_contestants) }
   end
 end
